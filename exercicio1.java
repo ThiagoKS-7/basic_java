706aca2596690c4fdb3ac30b8cebf2b643c1d32c
Thiago Kasper de Souza <83460816+ThiagoKS-7@users.noreply.github.com>
2022-03-22 20:54:54 -0300

    Create exercicio1.java

---
+++ exercicio1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+	  public static void main(String[]args) {
+	  Scanner t = new Scanner(System.in);
+		  int A,B,SOMA;
+		      A = t.nextInt();
+		      B = t.nextInt();
+		      SOMA = A + B;
+	  System.out.println("SOMA = " + SOMA);
+	  t.close();      
+	  }
+}
