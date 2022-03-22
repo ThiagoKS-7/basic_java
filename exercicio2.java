0b5413bdc1d383c83bb3ba0874bb127cc33339dd
Thiago Kasper de Souza <83460816+ThiagoKS-7@users.noreply.github.com>
2022-03-22 20:55:25 -0300

    Create exercicio2.java

---
+++ exercicio2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	  public static void main(String[]args) {
+	  Scanner t = new Scanner(System.in);
+		  int A,B,PROD;
+		      A = t.nextInt();
+		      B = t.nextInt();
+		      PROD = A * B;
+	  System.out.println("PROD = " + PROD);
+	  t.close();      
+	  }
